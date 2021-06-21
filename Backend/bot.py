9dd99a03ff20d61897562bec753bcc98d3815e1e
arnav0401 <65774796+arnav0401@users.noreply.github.com>
2021-06-22 00:00:14 +0800

    Update bot.py

--- Backend/bot.py
+++ Backend/bot.py
@@ -1,5 +1,6 @@
 import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+import random
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, ParseMode
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -8,17 +9,19 @@ from telegram.ext import (
     ConversationHandler,
     CallbackContext,
 )
-
-from dbhelper import DBHelper
-db = DBHelper()
-
 from model import recommend_movie
 from model import random_recommendation
 from model import genre_recommendation
+from model import recommend_movie_backend
 
-import os
-PORT = int(os.environ.get('PORT', 5000))
-TOKEN = "1869428334:AAEzLpVLdWVLjqxG3Z9T_bZXiY3MCDlEuh0"
+from config import *
+
+from faunadb import query as q
+from faunadb.client import FaunaClient
+from faunadb.errors import NotFound
+
+# Fauna Client Config
+client = FaunaClient(secret=FAUNA_KEY)
 
 # Enable logging
 logging.basicConfig(
@@ -30,30 +33,54 @@ logger = logging.getLogger(__name__)
 COMPANY, DEGREE, COLOUR, MOOD, FINAL, FINAL_2 = range(6)
 GENRE_2 = range(1)
 
-def start(update: Update, _: CallbackContext) -> int:
+def start(update, context: CallbackContext) -> int:
     ''' Starts the conversation by introducing itself '''
-    username = update.sendMessage(update.message.chat_id, str(update.message.from_user.username))
-    if db.check_user(username):
+    bot = context.bot
+    chat_id = update.message.from_user.username
+    if client.query(q.exists(q.match(q.index("userID"), chat_id))):
         update.message.reply_text(
             "Hi there! Welcome back to JustWatch!"
             "Please select your choice of command to kickstart this process :)"
-        )
+    )
     else:
         update.message.reply_text(
-            "Hello! This seems like your first time here. Press /help to learn how this bot works!"
+            "Hi there! This seems like your first time here. Press /help to learn how this bot works!"
         )
 
-def random(update: Update, _: CallbackContext) -> int:
+def help(update, context):
+    ''' Lists all the commands '''
+    bot = context.bot
+    chat_id = update.message.from_user.username
+    update.message.reply_text(
+        "Commands: \n" + "\n" +
+
+        "/start: Activates the bot \n" + "\n"
+        "/random: Recommends a random movie \n" + "\n"
+        "/genre: Recommends a random movie from the desired genre \n" + "\n"
+        "/personalized: Recommends a personalized movie \n" + "\n"
+        "/website: Returns a link that connects to our website \n" + "\n"
+        "/cancel: Deactivates the bot"
+    )
+
+def website(update, context):
+    bot = context.bot
+    chat_id = update.message.from_user.username
+    bot.send_message(chat_id=update.message.chat_id, 
+    text="<a href='https://justwatch01.herokuapp.com/'>JustWatch Website</a>", parse_mode=ParseMode.HTML)
+
+###### RANDOM RECOMMENDATION ######
+def random_rec(update: Update, _: CallbackContext) -> int:
     ''' Recommends a completely random movie '''
     update.message.reply_text(
         "Here is a random movie recommendation for you: " + random_recommendation()
     )
 
+###### GENRE RECOMMENDATION ###### 
 def genre(update: Update, _: CallbackContext) -> int:
     ''' Asks User to select a specific Genre'''
     user = update.message.from_user
     update.message.reply_text(
-        "Which movie genre would you like to watch?"
+        "From which Genre would you like to watch a movie?"
     )
     return GENRE_2
 
@@ -67,25 +94,54 @@ def genre_2(update: Update, _: CallbackContext) -> int:
     )
     return ConversationHandler.END
 
-
-def recommend(update: Update, _: CallbackContext) -> int:
+###### PERSONALISED RECOMMENDATION ###### 
+def personalized(update, context):
     ''' Starts the recommendation thread '''
-    reply_keyboard = [['Yes', 'No']]
-    update.message.reply_text(
-        "To recommend you the perfect movie, I would need you to answer few questions. "
-        #"So, let's get started. Type 'JustWatch' to begin!"
-        "Shall we begin?",
-        reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
-    ) 
+    bot = context.bot
+    chat_id = update.message.from_user.username
+
+    if not client.query(q.exists(q.match(q.index("userID"), chat_id))):
+        update.message.reply_text(
+            "To recommend you the perfect movie, I would need you to answer few questions for me! "
+            "Since, this is your first time, I would like to know your favourite 3 movies. "
+            "Please type them in separated by a comma. For example; Matrix, Godzilla, Meg." 
+        )
+    else:
+        update.message.reply_text(
+        "Since we have already recorded your most favourite movies, we can skip that step. "
+        "Please type 'next' to go to the next question! "
+        ) 
     return COMPANY
 
-def company(update: Update, _: CallbackContext) -> int:
-    ''' Asks the user who would they be watching the movie with? '''    
+def company(update, context):
+    ''' Asks the user who would they be watching the movie with? '''
+
+    bot = context.bot
+    chat_id = update.message.from_user.username
+
+    data = update.message.text.split(',')
+    
+    # Check if user already exists before creating new users
+    if not client.query(q.exists(q.match(q.index("userID"), chat_id))):
+        new_user = client.query(
+            q.create(q.collection('Users'), {
+                "data": {
+                    "userID": chat_id,
+                    "movie": data
+                }
+            })
+        )
+        context.user_data["userID"] = new_user["ref"].id()
+        context.user_data["movie"] = new_user['data']
+
+    # Asking next question
     reply_keyboard = [['Alone', 'Friends', 'Family', 'S/O']]
-    update.message.reply_text(
-        'Who are you intending to watch the movie with?',
+
+    if client.query(q.exists(q.match(q.index("userID"), chat_id))):
+        update.message.reply_text(
+        "Next question, Who are you intending to watch the movie with?",
         reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
-    )
+        )
     return DEGREE
 
 def degree(update: Update, _: CallbackContext) -> int:
@@ -127,39 +183,51 @@ def mood(update: Update, _: CallbackContext) -> int:
 def final(update: Update, _: CallbackContext) -> int:
     ''' recommends movie to the User '''
     user = update.message.from_user
+    chat_id = update.message.from_user.username
     logger.info("%s is currently feeling: %s", user.first_name, update.message.text)
-    if update.message.text == 'Excited':
-        update.message.reply_text(
-           "We recommend you: " + recommend_movie('Excited') + "!" + 
-           " Are you happy with this recommendation?" + " y or n"
-        )
-    elif update.message.text == 'Lonely':
-        update.message.reply_text(
-            "We recommend you: " + recommend_movie('Lonely') + "!" + 
-           " Are you happy with this recommendation?" + " y or n"
-        )
-    elif update.message.text == 'Depressed':
-        update.message.reply_text(
-           "We recommend you: " + recommend_movie('Depressed') + "!" +
-           " Are you happy with this recommendation?" + " y or n"
-        )
-    elif update.message.text == 'Cheerful':
-        update.message.reply_text(
-           "We recommend you: " + recommend_movie('Cheerful') + "!" +
-           " Are you happy with this recommendation?" + " y or n"
-        )
-    elif update.message.text == 'Stressed':
-        update.message.reply_text(
-           "We recommend you: " + recommend_movie('Stressed') + "!" + 
-             " Are you happy with this recommendation?" + " y or n"
-        )
+
+    result = client.query(
+    q.get(q.match(q.index("userID"), chat_id)))
+    output = result['data']['movie']
+    final_output = random.choice(output)
+
+    try:
+        recommend_movie_backend(final_output)
+    except:
+        if update.message.text == 'Excited':
+            update.message.reply_text(
+            "We recommend you: " + recommend_movie('Excited') + "!" + 
+            " Are you happy with this recommendation?" + " y or n"
+            )
+        elif update.message.text == 'Lonely':
+            update.message.reply_text(
+                "We recommend you: " + recommend_movie('Lonely') + "!" + 
+                " Are you happy with this recommendation?" + " y or n"
+            )
+        elif update.message.text == 'Depressed':
+            update.message.reply_text(
+                "We recommend you: " + recommend_movie('Depressed') + "!" +
+                " Are you happy with this recommendation?" + " y or n"
+            )
+        elif update.message.text == 'Cheerful':
+            update.message.reply_text(
+                "We reco mmend you: " + recommend_movie('Cheerful') + "!" +
+                " Are you happy with this recommendation?" + " y or n"
+            )
+        elif update.message.text == 'Stressed':
+            update.message.reply_text(
+                "We recommend you: " + recommend_movie('Stressed') + "!" + 
+                " Are you happy with this recommendation?" + " y or n"
+            )
 
     return FINAL_2
 
 def final_2(update: Update, _: CallbackContext) -> int:
     ''' Thanking Note '''
     user = update.message.from_user
+    chat_id = update.message.from_user.username
     logger.info("Did %s like the movie? : %s", user.first_name, update.message.text)
+
     update.message.reply_text(
             "Thank you for your feedback! This will allow us to constantly retrain our model for better results! " +
             " You can always get another recommendation by clicking on /cancel and then /start to reactive the bot!"
@@ -182,12 +250,14 @@ def main() -> None:
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("random", random))
-    #dispatcher.add_handler(CommandHandler("genre", genre))
+    dispatcher.add_handler(CommandHandler("random", random_rec))
+    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("cancel", cancel))
+    dispatcher.add_handler(CommandHandler("website", website))
 
     # Add conversation handler with the states
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('recommend', recommend)],
+        entry_points=[CommandHandler('personalized', personalized)],
         states={
             COMPANY: [MessageHandler(Filters.text & ~Filters.command, company)],
             DEGREE: [MessageHandler(Filters.text & ~Filters.command, degree)],
@@ -213,7 +283,6 @@ def main() -> None:
     # Start the Bot
     updater.start_polling()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
